47ca9e494834351b535771054b81d6c1098c225d
Ivabek33 <tiwanski@interia.pl>
2024-12-15 20:41:14 +0100

    Dodano funkcję do obliczania całki metodą trapezów

--- LiczbaPI/LiczbaPI.cpp
+++ LiczbaPI/LiczbaPI.cpp
@@ -20,6 +20,27 @@ double funkcja(double x) {
     return 4.0 / (1.0 + x * x);
 }
 
+/**
+ * @brief Oblicza wartosc calki dla podanego przedzialu metoda trapezow.
+ *
+ * @param poczatek Poczatek przedzialu calkowania.
+ * @param koniec Koniec przedzialu calkowania.
+ * @param kroki Liczba krokow podzialu przedzialu.
+ * @return double Wynik calki na przedziale [poczatek, koniec].
+ */
+double calkaFragment(double poczatek, double koniec, int kroki) {
+    double krokRozmiar = (koniec - poczatek) / kroki;
+    double wynik = 0.0;
+
+    for (int i = 0; i < kroki; ++i) {
+        double x1 = poczatek + i * krokRozmiar;
+        double x2 = x1 + krokRozmiar;
+        wynik += 0.5 * (funkcja(x1) + funkcja(x2)) * krokRozmiar;
+    }
+
+    return wynik;
+}
+
 int main()
 {
     
